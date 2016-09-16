e0e12cc341c4f9e1ffdd492afdafc84bea878037
Radoslaw Kieltyka <rkieltyka@dataplicity.com>
2016-09-16 13:43:37 +0000

    Added highly sophisticated password mechanism for the terminal

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ from terminal import Terminal
 import facebook
 import rpi_leds
 import datetime
+import argparse
 
 # Flask APP
 HOST = "0.0.0.0"
@@ -13,6 +14,16 @@ time_now = datetime.datetime.now()
 term = Terminal()
 fb = facebook.Facebook_messages()
 app = Flask(__name__)
+
+arg_parser = argparse.ArgumentParser(description="Facebook Terminal Server")
+arg_parser.add_argument("password", type=str, help="Password set for terminal access from facebook.")
+args = arg_parser.parse_args()
+
+# Commands
+TERM = "term "
+DATE = "date"
+TIME = "time"
+HELP = "help"
 
 @app.route('/', methods=['GET'])
 def subscription():
@@ -32,7 +43,7 @@ def subscription():
 @app.route('/', methods=['POST'])
 def webhook():
     """
-    This it the main function where facebook chat messages are sent and received.
+    This is the main function where facebook chat messages are sent and received.
     """
     data = request.get_json()
 
@@ -48,40 +59,41 @@ def webhook():
                     message_text = msg_event["message"]["text"]
 
                     # Help
-                    if message_text == "help" and term.term_started == False:
-                        fb.simple_msg(sender_id, "1. To enter into terminal mode please send command \"term\" (To exit use \"exit\" when in term mode)")
-                        fb.simple_msg(sender_id, "2. To see current time on a Raspberry Pi please enter command \"time\"")
-                        fb.simple_msg(sender_id, "3. To see current date on a Raspberry Pi please enter command \"date\"")
+                    if message_text == HELP and term.term_started == False:
+                        fb.simple_msg(sender_id, "1. Enter terminal mode by sending: " + TERM + "<password>"
+                                               + "\nTo exit terminal mode send: exit")
+                        fb.simple_msg(sender_id, "2. See current time on a Raspberry Pi by sending: " + TIME)
+                        fb.simple_msg(sender_id, "3. See current date on a Raspberry Pi by sending: " + DATE)
 
                     # Normal mode commands
-                    elif message_text == "date" and term.term_started == False:
+                    elif message_text == DATE and term.term_started == False:
                         fb.simple_msg(sender_id, time_now.strftime("%d-%m-%Y"))
-                        pass
 
-                    elif message_text == "time" and term.term_started == False:
+                    elif message_text == TIME and term.term_started == False:
                         fb.simple_msg(sender_id, time_now.strftime("%H:%M"))
-                        pass
 
                     # Terminal mode
-                    elif message_text == "term" and term.term_started == False:
+                    elif message_text == TERM + args.password and term.term_started == False:
                         term.start_terminal()
                         fb.simple_msg(sender_id, "<<< Term mode: Started >>>")
                         fb.simple_msg(sender_id, term.execute_command("pwd"))
 
-                    elif message_text == "term" and term.term_started == True:
+                    elif message_text == TERM + args.password and term.term_started == True:
                         fb.simple_msg(sender_id, "<<< Term mode: Already running >>>")
 
-                    elif message_text != "term" and term.term_started == True:
+                    elif message_text != TERM + args.password and term.term_started == True:
                         fb.simple_msg(sender_id, "<<< Term mode >>>")
                         fb.long_msg(sender_id, term.execute_command(message_text))
 
                     # Echo unrecognized commands
                     else:
                         fb.simple_msg(sender_id, "Command not found: " + message_text)
+                        fb.simple_msg(sender_id, "To view command help page send: " + HELP)
 
     return "ok", 200
 
 
 if __name__ == '__main__':
     print(" * Starting Server")
+    print(" * Facebook Terminal password: " + args.password)
     app.run(host=HOST, port=PORT)
