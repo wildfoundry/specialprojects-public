4d4f57d0c1f1581969d6f4955455e099300bb79d
Radoslaw Kieltyka <Radoslaw-K@users.noreply.github.com>
2019-12-18 15:41:31 +0000

    Add file_browser installer

---
+++ install_filebrowser.lua
@@ -0,0 +1,54 @@
+#!/usr/bin/env lua
+
+
+function check_superuser()
+    tmp = assert(io.popen('id -u', 'r')) 
+    current_user = assert(tmp:read('*a'))
+    tmp:close()
+    current_user = string.gsub(current_user, "\n", "")
+    root_user = '0'
+
+    if current_user ~= root_user then 
+        print('Please execute this script as root user.') 
+        os.exit()
+    end
+end
+
+
+function install_dependencies()
+    os.execute('apt install -y git')
+end
+
+
+function install_filebrowser()
+   os.execute('curl -fsSL https://filebrowser.xyz/get.sh | bash')
+   os.execute('filebrowser config set -p 80')
+end
+
+
+function generate_launcher()
+    os.execute('mkdir /opt/file_browser')
+
+    destination_dir = '/opt/file_browser/start'
+
+    launcher = io.open(destination_dir, 'w')
+    launcher:write([[
+#!/bin/sh
+filebrowser
+]])
+    launcher:close()
+
+    os.execute('chmod 755 ' .. destination_dir)
+end
+
+
+---------------------------------------------------------------------------------------------------
+
+
+check_superuser()
+
+install_dependencies()
+install_filebrowser()
+generate_launcher()
+
+print("End of installation")
